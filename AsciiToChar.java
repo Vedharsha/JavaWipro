2b86cacbfef8252aec6abefc6596b1136b3fd25e
Vedharsha <159508626+Vedharsha@users.noreply.github.com>
2025-07-06 13:11:43 +0530

    Add files via upload

---
+++ AsciiToChar.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class AsciiToChar {
+    public static void main(String[] args) {
+        int[] a={78,89,112,118,52};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print((char)a[i]+" ");
+        }
+    }
+}
